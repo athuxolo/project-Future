814ccb72d9fac858f3ec197ffafccd1a6e7e0f28
Athuxolo Ntenteni <vib.ntenteni@gmail.com>
2016-05-20 08:40:17 +0200

    dotnet-interest-project First Commit.

---
+++ dotnet-interest-project/dotnet-interest-project/Controllers/FoodController.cs
@@ -0,0 +1,48 @@
+﻿using dotnet_interest_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace dotnet_interest_project.Controllers
+{
+    [EnableCorsAttribute("*", "*", "*")]
+    public class FoodController : ApiController
+    {
+
+        // GET api/food
+        public IEnumerable<Food> Get()
+        {
+            var foodRepository = new FoodRepository();
+            return foodRepository.Retrieve();
+        }
+
+        // GET api/food/5
+        public Food Get(string name)
+        {
+            var foodRepository = new FoodRepository();
+            var products = foodRepository.Retrieve();
+            return products.First(p => p.Name.Equals(name));
+        }
+
+        // POST api/food
+        public void Post([FromBody]Food food)
+        {
+            var foodRepository = new FoodRepository();
+            foodRepository.Save(food);
+        }
+
+        // PUT api/food/5
+        public void Put(int id, [FromBody]Food value)
+        {
+        }
+
+        // DELETE api/food/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ dotnet-interest-project/dotnet-interest-project/Models/FoodRepository.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace dotnet_interest_project.Models
+{
+    public class FoodRepository
+    {
+        /// <summary>
+        /// Creates a new product with default values
+        /// </summary>
+        /// <returns></returns>
+        internal Food Create(string name, double price)
+        {
+            Food food = new Food(name, price); 
+            return food;
+        }
+
+        /// <summary>
+        /// Retrieves the list of products.
+        /// </summary>
+        /// <returns></returns>
+        internal List<Food> Retrieve()
+        {
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = System.IO.File.ReadAllText(filePath);
+
+            var products = JsonConvert.DeserializeObject<List<Food>>(json);
+
+            return products;
+        }
+
+        /// <summary>
+        /// Saves a new product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        internal Food Save(Food product)
+        {
+            // Read in the existing products
+            var products = this.Retrieve();
+
+            // Assign a new Id
+            //var maxId = products.Max(p => p.ProductId);
+            //product.ProductId = maxId + 1;
+
+            products.Add(product);
+
+            WriteData(products);
+            return product;
+        }
+
+        /// <summary>
+        /// Updates an existing product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        internal Food Save(int id, Food food)
+        {
+            // Read in the existing products
+            var products = this.Retrieve();
+
+            // Locate and replace the item
+            var itemIndex = products.FindIndex(p => p.Name == food.Name);
+            if (itemIndex > 0)
+            {
+                products[itemIndex] = food;
+            }
+            else
+            {
+                return null;
+            }
+
+            WriteData(products);
+            return food;
+        }
+
+        private bool WriteData(List<Food> products)
+        {
+            // Write out the Json
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = JsonConvert.SerializeObject(products, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, json);
+
+            return true;
+        }
+
+    }
+}
